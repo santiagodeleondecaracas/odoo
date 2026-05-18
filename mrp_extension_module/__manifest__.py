236f38d354fbc3fb6acbd4c7bee2385e1affa7bb
Aaron <aaron.argotte@itbcgroup.com>
2026-05-18 15:05:59 -0400

    [Fix]: Cambios en manifest

--- mrp_extension_module/__manifest__.py
+++ mrp_extension_module/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '17.0.1.0.0',
     'category': 'Manufacturing',
     'summary': 'Extiende stock_move_backdating para Órdenes de Producción',
+    'author': 'aaron_argotte@hotmail.com',
     'depends': [
         'mrp',
         'stock_account',
@@ -12,4 +13,9 @@
     ],
     'installable': True,
     'auto_install': False,
+    'price': 120.0,           # Required to display "on sale"
+    'currency': 'USD',        # Must be EUR or USD (Odoo preferred default is EUR)
+    'license': 'OPL-1',       # MANDATORY for paid apps. Open source licenses will break the pricing logic.
+    'application': True,      # Tells the app store to treat it as a standalone app
+    'installable': True,
 }
